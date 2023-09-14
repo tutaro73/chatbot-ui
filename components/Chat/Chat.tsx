d11028d921b28b48472428de80aca60c4a360af8
tutaro73 <81292652+tutaro73@users.noreply.github.com>
2023-09-14 16:58:47 +0900

    Update Chat.tsx
    Suzuki→C-ModLab

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,7 +352,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI　suzuki v1
+            Welcome to Chatbot UI　C-ModLab
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
